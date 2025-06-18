46eea244d84b1a1a3534d4e155c574252b3a40ab
MAX SIETSEMA <132317033+Justmax10@users.noreply.github.com>
2025-06-18 21:08:21 +0000

    first

---
+++ task.rb
@@ -0,0 +1,16 @@
+class Task
+  attr_accessor :description, :status
+
+  def initialize(description)
+    @description = description
+    @status = "incomplete"
+  end
+
+  def mark_complete
+    @status = "complete"
+  end
+
+  def to_s
+    "#{description} - #{status}"
+  end
+end
